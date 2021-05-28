67caf108827dd047c20fb35b19962a332955dffc
Martin Masevski <martin.masevski@gmail.com>
2021-05-28 12:55:57 +0200

    Added refresh button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
+
 function App() {
   const [people, setPeople] = useState(data);
+
+  if (people.length === 0) {
+    return (
+      <main>
+        <section className="container">
+          <h3>{people.length} Birtdays Today</h3>
+          <button onClick={() => setPeople(data)}>Refresh</button>
+        </section>
+      </main>
+    );
+  }
+
   return (
     <main>
       <section className="container">
